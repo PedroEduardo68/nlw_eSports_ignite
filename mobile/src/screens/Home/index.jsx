6ac9de4116c4ff542a9b4bd68c56c6c970f8a8d5
Pedro Eduardo Camera <pedro.eduardo.camera@gmail.com>
2022-09-13 20:34:22 -0300

    finaly interface home modle

---
+++ mobile/src/screens/Home/index.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { View, Image, FlatList } from 'react-native';
+
+import { styles } from './styles';
+import logoImg from '../../assets/logo-nlw-esports.png'
+import { Heading } from '../../components/Heading';
+import { GameCard } from '../../components/GameCard';
+
+import { GAMES } from '../../utils/games'
+
+
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={logoImg}
+        style={styles.logo}
+      />
+
+
+      <Heading 
+        title='Encontre seu DUO!!'
+        subtitle='Selecione o game que deseja Jogar..'
+      />
+
+      <FlatList 
+        data={GAMES}
+        keyExtractor={item => item.id}
+        renderItem={({item}) =>(
+            <GameCard 
+            data={item}
+          />
+        )}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        contentContainerStyle={styles.contentList}
+      />
+
+
+
+    </View>
+  );
+}
