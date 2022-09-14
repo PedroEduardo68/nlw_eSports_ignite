eb691355a752b327d9f67206de6c1507a9d2354a
Pedro Eduardo Camera <pedro.eduardo.camera@gmail.com>
2022-09-14 20:05:49 -0300

    1

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,7 @@
 import './styles/main.css'
 import logoImg from './assets/logo-nlw-esports.svg'
 import {MagnifyingGlassPlus} from 'phosphor-react'
+import { GameBanner } from './components/GameBanner'
 
 function App() {
   return (
@@ -10,66 +11,8 @@ function App() {
       <h1 className="text-6xl text-white font-black mt-20 ">Seu <span className="bg-nlw-gradient text-transparent bg-clip-text">duo</span> está aqui...</h1>
 
       <div className='grid grid-cols-6 gap-6 mt-16'>
-        <a href="" className='relative rounded-lg overflow-hidden'>
-          <img src="/game-1.png" alt="game"/>
-
-          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-            <strong className="font-bold text-white block">League of legends</strong>
-            <span className="text-zinc-300 text-sm block mt-1">4 anúncios</span>
-          </div>
-        </a>
-
-
-        <a href="" className='relative rounded-lg overflow-hidden'>
-          <img src="/game-2.png" alt="game"/>
-
-          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-            <strong className="font-bold text-white block">Dota 2</strong>
-            <span className="text-zinc-300 text-sm block mt-1">4 anúncios</span>
-          </div>
-        </a>
-
-
-
-        <a href="" className='relative rounded-lg overflow-hidden'>
-          <img src="/game-3.png" alt="game"/>
-
-          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-            <strong className="font-bold text-white block">CS GO</strong>
-            <span className="text-zinc-300 text-sm block mt-1">4 anúncios</span>
-          </div>
-        </a>
-
-
-        <a href="" className='relative rounded-lg overflow-hidden'>
-          <img src="/game-4.png" alt="game"/>
-
-          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-            <strong className="font-bold text-white block">APEX</strong>
-            <span className="text-zinc-300 text-sm block mt-1">4 anúncios</span>
-          </div>
-        </a>
-
-
-        <a href="" className='relative rounded-lg overflow-hidden'>
-          <img src="/game-5.png" alt="game"/>
-
-          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-            <strong className="font-bold text-white block">Fortnite</strong>
-            <span className="text-zinc-300 text-sm block mt-1">4 anúncios</span>
-          </div>
-        </a>
-
-
-        <a href="" className='relative rounded-lg overflow-hidden'>
-          <img src="/game-6.png" alt="game"/>
-
-          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-            <strong className="font-bold text-white block">Warcraft</strong>
-            <span className="text-zinc-300 text-sm block mt-1">4 anúncios</span>
-          </div>
-        </a>
-
+        
+        <GameBanner bannerUrl="/game-1.png" title="lol" adsCount={2} />
 
 
       </div>
