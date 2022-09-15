100231f719e4a54d23b83d988068e2887db23dd7
Pedro Eduardo Camera <pedro.eduardo.camera@gmail.com>
2022-09-14 21:52:06 -0300

    3

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
+
 import { View, Image, FlatList } from 'react-native';
 
 import { styles } from './styles';
 import logoImg from '../../assets/logo-nlw-esports.png'
 import { Heading } from '../../components/Heading';
-import { GameCard } from '../../components/GameCard';
+import { GameCard, GameCardProps } from '../../components/GameCard';
 
 import { GAMES } from '../../utils/games'
+import { useEffect, useState } from 'react';
+
+
 
 
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([])
+
+
+  useEffect(() =>{
+    fetch('http://192.168.18.145:333/games')
+      .then(response =>response.json())
+      .then(data => setGames(data))
+  }, [])
   return (
     <View style={styles.container}>
       <Image
@@ -25,7 +36,7 @@ export function Home() {
       />
 
       <FlatList 
-        data={GAMES}
+        data={games}
         keyExtractor={item => item.id}
         renderItem={({item}) =>(
             <GameCard 
