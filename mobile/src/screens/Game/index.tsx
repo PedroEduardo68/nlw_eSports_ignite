08a83444c9ca3e75f695b87315be94c9609e246b
Pedro Eduardo Camera <pedro.eduardo.camera@gmail.com>
2022-09-14 22:46:43 -0300

    7

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,14 +1,24 @@
 
 import {SafeAreaView} from 'react-native-safe-area-context'
+import { Background } from '../../components/Background';
+import { useRoute } from '@react-navigation/native';
+
 
 
 
 import { styles } from './styles';
+import { GameParams } from '../../@types/navigation';
 
 export function Game() {
-  return (
-    <SafeAreaView style={styles.container}>
 
-    </SafeAreaView>
+  const route = useRoute();
+  const game = route.params as GameParams;
+  
+  return (
+    <Background>
+        <SafeAreaView style={styles.container}>
+
+        </SafeAreaView>
+    </Background>
   );
 }
