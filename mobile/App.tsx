f1b7801257bf04299892c8b9ca385eda162bfe34
Pedro Eduardo Camera <pedro.eduardo.camera@gmail.com>
2022-09-18 14:32:11 -0300

    3

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,13 +1,48 @@
 
-import { StatusBar } from 'react-native';
+import { StatusBar, SubscribableMixin } from 'react-native';
+import { useRef, useEffect } from 'react';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black } from '@expo-google-fonts/inter';
 import { Background } from './src/components/Background';
 import {Routes} from './src/Routes'
 import { Loading } from './src/components/Loading';
-
+import {Subscription} from 'expo-modules-core'
+import * as Notifications from 'expo-notifications'
+import './src/services/notificationsConfigs'
+import {getPushNotificationToken} from './src/services/getPushNotificationToken'
 
 
 export default function App() {
+
+  const getNotificationListener = useRef<Subscription>()
+  const responseNotificationListener = useRef<Subscription>()
+
+
+  useEffect(()=>{
+    getPushNotificationToken();
+  })
+
+
+  useEffect(()=>{
+    getNotificationListener.current = Notifications.addNotificationResponseReceivedListener(notification =>{
+      console.log(notification)
+    })
+
+    responseNotificationListener.current = Notifications.addNotificationResponseReceivedListener(resposne =>{
+      console.log(resposne)
+    })
+
+
+    return () =>{
+      if (getNotificationListener.current && responseNotificationListener.current){
+        Notifications.removeNotificationSubscription(getNotificationListener.current)
+        Notifications.removeNotificationSubscription(responseNotificationListener.current)
+      }
+    }
+  })
+
+
+
+
   const [fontsLoaded] = useFonts({
     Inter_400Regular, 
     Inter_600SemiBold, 
